7a857fb1d2cc674347d5f68126f7c73609696cf2
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2016-08-25 06:28:33 -0500

    first commit

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Rickey Hrabowskie on 8/24/16.
+//  Copyright © 2016 Rickey Hrabowskie. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> () // Creating Closure 
---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,33 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Rickey Hrabowskie on 8/22/16.
+//  Copyright © 2016 Rickey Hrabowskie. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0 // Rounded corners 
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    
+}
